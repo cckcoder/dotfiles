b8e2d38592792874d04a2d9b94bb9b41d980171d
codewiz <c.chaiwat.ch@gmail.com>
2020-07-08 07:33:45 +0700

    remove fish

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-#GIT
-alias gst='git status'
-
-#General
-alias ppp='cd /opt/youtube/porn'
-alias dot='cd ~/dotfiles/'
-alias dow='cd ~/Downloads/'
-alias cfv='vi ~/dotfiles/.vimrc'
-alias cff='vi ~/dotfiles/fish/config.fish'
