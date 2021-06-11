038c49ca95ae85b2c9d0f1a06bea0c8fe3bcfde5
codewiz <c.chaiwat.ch@gmail.com>
2021-06-11 15:34:46 +0700

    add: lsp html

--- get_tools.sh
+++ get_tools.sh
@@ -1,6 +1,4 @@
 pacman -S ripgrep gawk
 
 npm i -g bash-language-server vscode-css-languageserver-bin pyright typescript typescript-language-server \
-    intelephense bash-language-server vim-language-server
-
-
+    intelephense bash-language-server vim-language-server vscode-html-languageserver-bin
