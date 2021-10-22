bc57925870873d0279ac5f874f6b0e3f839cc6e4
dev2 <c.chaiwat.ch@gmail.com>
2021-10-22 14:29:41 +0700

    update: nvim-tree follow by file.

--- lua/plug_in/NvimTree.lua
+++ lua/plug_in/NvimTree.lua
@@ -1,10 +1,19 @@
 vim.g.nvim_tree_indent_markers = 1
 
 require("nvim-tree").setup({
-	disable_netrw = false,
-	update_cwd = true,
+	disable_netrw = true,
+	hijack_netrw = true,
+	update_to_buf_dir = {
+		enable = true,
+		auto_open = true,
+	},
 	auto_close = true,
+	update_cwd = true,
 	view = {
 		side = "right",
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
 	},
 })
